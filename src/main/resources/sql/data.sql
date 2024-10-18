3b69a7531810eb3fbc021e61a3408b4d095a31ef
jokessell <jokessell@teksystems.com>
2024-10-18 14:19:08 -0500

    Refactored database for more even distribution of skills, projects, and employees

--- src/main/resources/sql/data.sql
+++ src/main/resources/sql/data.sql
@@ -69,92 +69,160 @@ INSERT INTO PROJECT_TBL (PROJECT_NAME, DESCRIPTION) VALUES
 
 -- Associate Employees with Projects
 INSERT INTO EMPLOYEE_PROJECT_TBL (EMPLOYEE_ID, PROJECT_ID) VALUES
-   (1, 1),
-   (1, 2),
-   (2, 4),
-   (3, 2),
-   (4, 3),
-   (5, 3),
-   (6, 1),
-   (7, 1),
-   (8, 5),
-   (9, 4),
-   (10, 2),
-   (11, 1),
-   (12, 6),
-   (13, 6),
-   (14, 7),
-   (15, 1),
-   (16, 8),
-   (17, 9),
-   (18, 10),
-   (19, 4),
-   (20, 5);
+-- Project Gamma
+(1, 1),
+(11, 1),
+-- Project Delta
+(2, 2),
+(12, 2),
+(13, 2),
+-- Project Epsilon
+(4, 3),
+(14, 3),
+(16, 3),
+-- Project Zeta
+(2, 4),
+-- Project Eta
+(7, 5),
+(8, 5),
+(17, 5),
+(18, 5),
+-- Project Theta
+(3, 6),
+(14, 6),
+-- Project Iota
+(8, 7),
+(9, 7),
+(19, 7),
+-- Project Kappa
+(10, 8),
+(11, 8),
+-- Project Lambda
+(5, 9),
+-- Project Mu
+(4, 10),
+(15, 10),
+(16, 10);
+
 
 -- Associate Employees with Skills
 INSERT INTO EMPLOYEE_SKILL_TBL (EMPLOYEE_ID, SKILL_ID) VALUES
-   (1, 1),
-   (1, 2),
-   (1, 3),
-   (2, 2),
-   (2, 5),
-   (2, 6),
-   (3, 4),
-   (3, 13),
-   (4, 12),
-   (4, 16),
-   (5, 3),
-   (5, 10),
-   (6, 1),
-   (6, 17),
-   (7, 1),
-   (7, 2),
-   (8, 8),
-   (8, 9),
-   (9, 2),
-   (9, 5),
-   (10, 4),
-   (10, 13),
-   (11, 1),
-   (11, 2),
-   (12, 12),
-   (12, 16),
-   (13, 12),
-   (13, 16),
-   (14, 3),
-   (14, 10),
-   (15, 1),
-   (15, 2),
-   (16, 1),
-   (16, 2),
-   (17, 1),
-   (17, 2),
-   (18, 2),
-   (18, 5),
-   (19, 2),
-   (19, 5),
-   (20, 5),
-   (20, 12);
+-- Alice Johnson
+(1, 1),  -- Java
+(1, 2),  -- Spring Boot
+(1, 3),  -- React
+-- Bob Smith
+(2, 4),  -- SQL
+(2, 5),  -- Docker
+(2, 6),  -- Kubernetes
+(2, 7),  -- AWS
+-- Charlie Davis
+(3, 8),  -- Python
+(3, 9),  -- Machine Learning
+-- Diana Evans
+(4, 10), -- UI/UX Design
+(4, 11), -- DevOps
+(4, 12), -- Project Management
+(4, 13), -- Quality Assurance
+(4, 14), -- Git
+-- Evan Green
+(5, 15), -- Jenkins
+-- Fiona Harris
+(6, 16), -- Agile Methodologies
+(6, 17), -- REST APIs
+-- George King
+(7, 18), -- Microservices
+(7, 19), -- NoSQL
+(7, 20), -- Security
+-- Hannah Lee
+(8, 21), -- JavaScript
+(8, 22), -- TypeScript
+(8, 23), -- C#
+(8, 24), -- C++
+-- Ian Moore
+(9, 25), -- Ruby
+(9, 26), -- Go
+-- Jasmine Patel
+(10, 27), -- PHP
+-- Kevin Quinn
+(11, 28), -- Swift
+(11, 29), -- Scala
+(11, 30), -- Perl
+(11, 1),  -- Java
+(11, 2),  -- Spring Boot
+-- Laura Roberts
+(12, 3),  -- React
+(12, 4),  -- SQL
+-- Michael Scott
+(13, 5),  -- Docker
+(13, 6),  -- Kubernetes
+(13, 7),  -- AWS
+-- Nina Turner
+(14, 8),  -- Python
+(14, 9),  -- Machine Learning
+(14, 10), -- UI/UX Design
+(14, 11), -- DevOps
+-- Oscar Vega
+(15, 12), -- Project Management
+-- Paula White
+(16, 13), -- Quality Assurance
+(16, 14), -- Git
+(16, 15), -- Jenkins
+(16, 16), -- Agile Methodologies
+(16, 17), -- REST APIs
+-- Quentin Brown
+(17, 18), -- Microservices
+(17, 19), -- NoSQL
+(17, 20), -- Security
+-- Rachel Adams
+(18, 21), -- JavaScript
+(18, 22), -- TypeScript
+-- Steven Clark
+(19, 23), -- C#
+(19, 24), -- C++
+(19, 25), -- Ruby
+(19, 26), -- Go
+-- Tina Davis
+(20, 27); -- PHP
+
 
 -- Associate Projects with Skills
 INSERT INTO PROJECT_SKILL_TBL (PROJECT_ID, SKILL_ID) VALUES
- (1, 1), -- Java
- (1, 2), -- Spring Boot
- (2, 4), -- SQL
- (2, 13), -- Quality Assurance
- (3, 3), -- React
+ -- Project Gamma
+ (1, 1),  -- Java
+ (1, 2),  -- Spring Boot
+ (1, 3),  -- React
+ -- Project Delta
+ (2, 4),  -- SQL
+ (2, 5),  -- Docker
+ -- Project Epsilon
  (3, 10), -- UI/UX Design
- (4, 2), -- Spring Boot
- (4, 5), -- Docker
- (5, 2), -- Spring Boot
- (5, 6), -- Kubernetes
- (6, 2), -- Spring Boot
- (6, 16), -- Agile Methodologies
- (7, 8), -- Python
- (7, 9), -- Machine Learning
- (8, 1), -- Java
- (8, 2), -- Spring Boot
- (9, 1), -- Java
- (9, 17), -- REST APIs
- (10, 5), -- Docker
- (10, 12); -- Project Management
+ (3, 11), -- DevOps
+ (3, 14), -- Git
+ (3, 16), -- Agile Methodologies
+ -- Project Zeta
+ (4, 7),  -- AWS
+ -- Project Eta
+ (5, 18), -- Microservices
+ (5, 19), -- NoSQL
+ (5, 20), -- Security
+ (5, 21), -- JavaScript
+ (5, 22), -- TypeScript
+ -- Project Theta
+ (6, 8),  -- Python
+ (6, 9),  -- Machine Learning
+ -- Project Iota
+ (7, 23), -- C#
+ (7, 24), -- C++
+ (7, 26), -- Go
+ -- Project Kappa
+ (8, 27), -- PHP
+ (8, 28), -- Swift
+ -- Project Lambda
+ (9, 15), -- Jenkins
+ -- Project Mu
+ (10, 11), -- DevOps
+ (10, 12), -- Project Management
+ (10, 13), -- Quality Assurance
+ (10, 14); -- Git
+
